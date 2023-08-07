be32234cbc269a1b4904e0fd689a8a66a306f65e
Derrbal <bderbal07@gmail.com>
2023-08-07 15:26:08 +0300

    updates

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -3,26 +3,16 @@ import * as path from "path"
 import * as fs from 'fs';
 import { expect, test } from "@playwright/test";
 
-test('using fixture context', async ({ page }) => {
-  await page.goto('http://localhost:3030/login?pageSize=10');
-  expect(await page.evaluate(() => window.innerWidth)).toBe(100);
-  expect(await page.evaluate(() => navigator.userAgent)).toBe('some custom ua');
-  await expect(page).toHaveURL('http://localhost:3030/login?pageSize=10');
-});
-
-test.only('manually created context', async ({ browser }, testInfo) => {
+test('manually created context', async ({ browser }, testInfo) => {
   const videoDir = path.join(testInfo.outputPath(), 'videos');
-  // const videoPath = path.join(testInfo.outputPath(), 'videos', `${testInfo.workerIndex}-${testInfo.retry}.webm`);
-  // console.log(videoPath);
   const context = await browser.newContext({ recordVideo: { dir: videoDir } });
   const page = await context.newPage();
   const page2 = await context.newPage();
   try {
     await page2.goto('http://localhost:3030/login?pageSize=10');
     await page.goto('http://localhost:3030/login?pageSize=10');
-    // expect(await userOnePage.evaluate(() => window.innerWidth)).toBe(100);
-    // expect(await userOnePage.evaluate(() => navigator.userAgent)).toBe('some custom ua');
     await expect(page).toHaveURL('http://localhost:3030/login?pageSize=10');
+    await page.waitForTimeout(5000);
   } finally {
     await page.context().close();
     const videoFiles = fs.readdirSync(videoDir);
@@ -35,3 +25,23 @@ test.only('manually created context', async ({ browser }, testInfo) => {
     }
   }
 });
+
+test.only('visual testing', async ({ page }) => {
+  await page.goto('http://localhost:3030/login?pageSize=10');
+  await expect(page).toHaveURL('http://localhost:3030/login?pageSize=10');
+  await page.waitForTimeout(5000);
+  const element = page.locator('section.fadeInUp') ;
+  await expect(element).toHaveScreenshot();
+  // const screenshot = await element.screenshot({ path: 'screenshot.png' });
+  // const newScreenshot = await element.screenshot();
+  // await screenshot.save('actual.png');
+  // const comparisonResult = await page.compareScreenshots(referenceScreenshot, newScreenshot);
+
+});
+
+// test('using fixture context', async ({ page }) => {
+//   await page.goto('http://localhost:3030/login?pageSize=10');
+//   expect(await page.evaluate(() => window.innerWidth)).toBe(100);
+//   expect(await page.evaluate(() => navigator.userAgent)).toBe('some custom ua');
+//   await expect(page).toHaveURL('http://localhost:3030/login?pageSize=10');
+// });
